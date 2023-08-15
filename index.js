caf152b9f90a16655a722f35d6c36a7151a43771
Nishant <nishanttomar910@gmail.com>
2023-08-15 15:15:25 +0530

    Minor change to js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function fetchWeatherData(location) {
 
 // Fetch weather data on document load for default location (Germany)
 document.addEventListener('DOMContentLoaded', () => {
-    const defaultLocation = 'Khordha';
+    const defaultLocation = 'Bhubaneswar';
     fetchWeatherData(defaultLocation);
 });
 
